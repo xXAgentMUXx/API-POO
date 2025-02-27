fde1a2cd662de90f9934fa352abd54dee6173925
xXAgentMUXx <mathys.urban@ynov.com>
2025-02-27 10:32:25 +0100

    Update GraduateStudent.java

--- demo/src/main/java/com/example/demo/GraduateStudent.java
+++ demo/src/main/java/com/example/demo/GraduateStudent.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("Graduate")
 public class GraduateStudent extends Student {
      // Default constructor
     public GraduateStudent() {}
@@ -13,7 +15,11 @@ public class GraduateStudent extends Student {
     // Method to modify the behavior for GraduateStudent
     @Override
     public double getAverageGrade() {
-        return super.getAverageGrade() + 2;
+        double average = super.getAverageGrade() + 2;
+        if (average >= 20) {
+            return 20;
+        }
+        return average;
     }
     public static void main(String[] args) {
         GraduateStudent gradeStudent = new GraduateStudent("Vito", 25, 2001);
