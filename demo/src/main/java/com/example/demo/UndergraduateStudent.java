b73b003dffbe6327b945188e773abe0ab8ce0903
xXAgentMUXx <mathys.urban@ynov.com>
2025-02-27 16:53:13 +0100

    Update UndergraduateStudent.java

--- demo/src/main/java/com/example/demo/UndergraduateStudent.java
+++ demo/src/main/java/com/example/demo/UndergraduateStudent.java
@@ -8,19 +8,21 @@ import jakarta.persistence.Entity;
 public class UndergraduateStudent extends Student {
     // Default constructor
     public UndergraduateStudent() {}
-     // Constructor to initialize the properties
+
+    // Constructor to initialize the properties
     public UndergraduateStudent(String name, int age, int studentID) {
         super(name, age, studentID);
+        this.setGraduated(false); 
     }
-    // Method to modify the behavior for UndergraduateStudent
+    // Method for the average grade for undergraduated
     @Override
     public double getAverageGrade() {
-        double average = super.getAverageGrade() - 2;
-        if (average <= 0) {
-            return 0;
-        }
-            return average;
-        }
+    return Math.max(super.getAverageGrade() - 2, 0); 
+    }
+    // Method to go to graudate at underGraduate
+    public void upgradeToGraduate() {
+        this.setGraduated(true); 
+    }
     public static void main(String[] args) {
         UndergraduateStudent gradeStudent = new UndergraduateStudent("Mathys", 25, 2001);
         gradeStudent.addGrade(17.9);
